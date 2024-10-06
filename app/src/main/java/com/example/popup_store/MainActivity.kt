27f207917feecb152af60c44378dc1da37ce968f
jiyoung <qkrwldud0603@gmail.com>
2024-10-07 01:30:32 +0900

    Popsonal

--- app/src/main/java/com/example/popup_store/MainActivity.kt
+++ app/src/main/java/com/example/popup_store/MainActivity.kt
@@ -14,6 +14,7 @@ import com.example.popup_store.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,24 +40,28 @@ class MainActivity : AppCompatActivity() {
                         .commit()
                     true
                 }
+
                 R.id.fragment_map -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fragment_container, MapFragment())
                         .commit()
                     true
                 }
+
                 R.id.fragment_event -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fragment_container, EventFragment())
                         .commit()
                     true
                 }
-                R.id.fragment_mypage -> {
+
+                R.id.fragment_myPage -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fragment_container, MyPageFragment())
                         .commit()
                     true
                 }
+
                 else -> false
             }
         }
